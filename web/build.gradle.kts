b5cfc463110d8f0b53ffccdc47a8a655d44e53c1
Valar <valarpiraichandran@Valarpiraichandrans-MacBook-Pro.local>
2025-02-22 22:28:44 +0530

    Initial commit

---
+++ web/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    id("java")
+}
+
+group = "com.valarpirai"
+version = "unspecified"
+
+jetty_version="11.0.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    // https://mvnrepository.com/artifact/org.eclipse.jetty/jetty-servlet
+    implementation("org.eclipse.jetty:jetty-server:11.0.0")
+    implementation("org.eclipse.jetty:jetty-servlet:11.0.0")
+    implementation("org.eclipse.jetty:jetty-webapp:11.0.0")
+    implementation("org.eclipse.jetty:jetty-websocket:11.0.0")
+
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ web/src/main/java/com/wizard/WizardApplication.kt
@@ -0,0 +1,4 @@
+package com.wizard
+
+class WizardApplication {
+}
