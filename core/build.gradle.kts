e2985965d45cf923935eb2421b6338c96feb59db
Valar <a.valarpirai@gmail.com>
2025-02-24 22:15:00 +0530

    Initial tests

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -10,6 +10,7 @@ dependencies {
 
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+
 }
 
 tasks.test {
--- core/src/test/kotlin/com/wizard/WizardServiceTest.kt
+++ core/src/test/kotlin/com/wizard/WizardServiceTest.kt
@@ -1,11 +1,26 @@
 package com.wizard
 
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.BeforeEach
 
 class WizardServiceTest {
     private var service: WizardService? = null
 
-    @Before
-    fun test() {
-        service =
+    @BeforeEach
+    fun setUp() {
+        service = WizardService.getInstance()
+    }
+
+    @AfterEach
+    fun tearDown() {
+    }
+
+    companion object {
+        @JvmStatic
+        @BeforeAll
+        fun test(): Unit {
+            var service = WizardService.getInstance()
+        }
     }
 }
